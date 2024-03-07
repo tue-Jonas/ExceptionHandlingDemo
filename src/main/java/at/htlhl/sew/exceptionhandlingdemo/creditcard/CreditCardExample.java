4932c0b0b8141776d874a5c3bad551f6d4b73636
Jonas Tüchler <jonas.tuechler@gmail.com>
2024-03-07 08:17:00 +0100

    uncaught exception essage

--- src/main/java/at/htlhl/sew/exceptionhandlingdemo/creditcard/CreditCardExample.java
+++ src/main/java/at/htlhl/sew/exceptionhandlingdemo/creditcard/CreditCardExample.java
@@ -11,7 +11,7 @@ public class CreditCardExample {
         Thread.currentThread().setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
             @Override
             public void uncaughtException(Thread t, Throwable e) {
-                System.out.println("Error: Invalid input. Please enter a number.");
+                System.out.println("Uncaught exception: " + e.getMessage());
             }
         });
 
